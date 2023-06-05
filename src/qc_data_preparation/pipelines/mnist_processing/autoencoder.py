030e2d0ba2fe0c3fe7ce22731d8435e5e1748a7e
lc3267 <melvin.strobl@kit.edu>
2023-06-05 17:40:23 +0200

    pytorch ae model

--- src/qc_data_preparation/pipelines/mnist_processing/autoencoder.py
+++ src/qc_data_preparation/pipelines/mnist_processing/autoencoder.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
+import torch as pt
 
-
-class Autoencoder(tf.keras.models.Model):
+class TF_Autoencoder(tf.keras.models.Model):
     def __init__(self, latent_dim):
-        super(Autoencoder, self).__init__()
+        super(TF_Autoencoder, self).__init__()
         self.latent_dim = latent_dim
         self.encoder = tf.keras.Sequential(
             [
@@ -35,6 +35,52 @@ class Autoencoder(tf.keras.models.Model):
         )
 
     def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+class PT_Autoencoder(pt.nn.Module):
+    """Autoencoder with 3 hidden layers."""
+
+    def __init__(self, latent_space_dim):
+        super(PT_Autoencoder, self).__init__()
+        self.encoder = pt.nn.Sequential(
+            pt.nn.Conv2d(
+                1, 16, 3, stride=2, padding=1
+            ),  # input size = 1x28x28 -> hidden size = 16x14x14
+            pt.nn.ReLU(True),
+            pt.nn.Conv2d(
+                16, 32, 3, stride=2, padding=1
+            ),  # hidden size = 16x14x14 -> hidden size = 32x7x7
+            pt.nn.ReLU(True),
+            pt.nn.Conv2d(32, 64, 7),  # hidden size = 32x7x7 -> hidden size = 64x1x1
+            pt.nn.Tanh(),
+            pt.nn.Flatten(),
+            pt.nn.Linear(64, 16),  # hidden size = 64x1x1 -> hidden size = latent_space_dim x1x1
+            pt.nn.Tanh(),
+            pt.nn.Linear(16, latent_space_dim),  # hidden size = 64x1x1 -> hidden size = latent_space_dim x1x1
+            pt.nn.Tanh()
+        )
+
+        self.decoder = pt.nn.Sequential(
+            pt.nn.Linear(latent_space_dim, 16),  # hidden size = latent_space_dim x1x1 -> hidden size = 64x1x1
+            pt.nn.ReLU(True),
+            pt.nn.Linear(16, 64),  # hidden size = 64x1x1 -> hidden size = latent_space_dim x1x1
+            pt.nn.ReLU(True),
+            pt.nn.Unflatten(1, (64, 1, 1)),
+            pt.nn.ConvTranspose2d(64, 32, 7),  # input size = latent_space_dim x1x1 -> hidden size = 32x7x7
+            pt.nn.ReLU(True),
+            pt.nn.ConvTranspose2d(
+                32, 16, 3, stride=2, padding=1, output_padding=1
+            ),  # hidden size = 32x7x7 -> hidden size = 16x14x14
+            pt.nn.ReLU(True),
+            pt.nn.ConvTranspose2d(
+                16, 1, 3, stride=2, padding=1, output_padding=1
+            ),  # hidden size = 16x14x14 -> hidden size = 1x28x28
+            pt.nn.Sigmoid(),  # output with pixels in [0,1]
+        )
+
+    def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
