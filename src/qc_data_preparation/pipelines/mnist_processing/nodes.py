8193039cac8c3b0121683bff5e69044bfe199372
lc3267 <melvin.strobl@kit.edu>
2023-06-07 10:51:58 +0200

    gc

--- src/qc_data_preparation/pipelines/mnist_processing/nodes.py
+++ src/qc_data_preparation/pipelines/mnist_processing/nodes.py
@@ -223,13 +223,6 @@ def generate_loss_curve(history: Dict):
     loss_val = history["val_loss"]
     epochs = range(1, len(list(history.values())[0]) + 1)
 
-    # plt.plot(epochs, loss_train, "g", label="Training loss")
-    # plt.plot(epochs, loss_val, "b", label="Validation loss")
-    # plt.title("Training and Validation loss")
-    # plt.xlabel("Epochs")
-    # plt.ylabel("Loss")
-    # plt.legend()
-
     plt = go.Figure(
         [
             go.Scatter(
@@ -259,13 +252,6 @@ def generate_ssim_curve(history: Dict):
     ssim_train = history["ssim"]
     ssim_val = history["val_ssim"]
     epochs = range(1, len(list(history.values())[0]) + 1)
-
-    # plt.plot(epochs, loss_train, "g", label="Training SSIM")
-    # plt.plot(epochs, loss_val, "b", label="Validation SSIM")
-    # plt.title("Training and Validation SSIM")
-    # plt.xlabel("Epochs")
-    # plt.ylabel("SSIM")
-    # plt.legend()
 
     plt = go.Figure(
         [
